6b6d6eb1bdf380a1a466bfd588825745099e61a1
Felipe Gavilán <gavilanch@hotmail.com>
2026-03-05 10:24:37 -0400

    Actualizacion

---
+++ ASP.NET Core 10/Modulo 3 - Razor/IntroduccionARazor/IntroduccionARazor/Utilidades/UtilidadesString.cs
@@ -0,0 +1,6 @@
+﻿namespace IntroduccionARazor.Utilidades;
+
+public class UtilidadesString
+{
+    public static string VolverMayusculas(string valor) => valor.ToUpper();
+}
---
+++ ASP.NET Core 10/Modulo 4 - Componentes/BlazorPeliculas/AppState.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorPeliculas;
+
+public class AppState
+{
+    public string Color { get; set; } = "red";
+}
+
+public class AppStateService
+{
+    public AppState AppState { get; set; } = new();
+
+    public CascadingValueSource<AppState> Source { get; }
+
+    public AppStateService()
+    {
+        Source = new CascadingValueSource<AppState>(AppState, isFixed: false);
+    }
+
+    public Task NotifyChangedAsync() => Source.NotifyChangedAsync();
+}
